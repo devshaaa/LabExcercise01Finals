4e9ee396683d85ceecea84cff59db254a53f144c
devshaaa <shnjnmrgll@gmail.com>
2023-11-17 08:44:26 +0800

    Promises and hopes, but no plans

--- src/myclass1/Myclass1.java
+++ src/myclass1/Myclass1.java
@@ -5,6 +5,8 @@ public class Myclass1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello to my dark side.");
+        System.out.println("Someday.");
+        System.out.println("Everything will be alright.");
     }
     
 }
