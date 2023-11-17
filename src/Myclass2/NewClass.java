04bbc09a9eac791770132680bd985f058b43d816
devshaaa <shnjnmrgll@gmail.com>
2023-11-17 08:36:07 +0800

    sakit nas ulo

---
+++ src/Myclass2/NewClass.java
@@ -0,0 +1,11 @@
+
+package Myclass2;
+
+
+public class NewClass {
+    
+     public static void main(String[] args) {
+         
+     System.out.println("The more we deny that we have a dark side, the more power it has over us.");
+     }
+}
---
+++ src/myclass1/Myclass1.java
@@ -0,0 +1,10 @@
+package myclass1;
+
+public class Myclass1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello to my dark side.");
+    }
+    
+}
