522db9c5fe78585ca3ed09cb802b0c9c338560b4
devshaaa <shnjnmrgll@gmail.com>
2023-11-17 08:40:35 +0800

    shhhhh

--- src/Myclass2/NewClass.java
+++ src/Myclass2/NewClass.java
@@ -6,6 +6,6 @@ public class NewClass {
     
      public static void main(String[] args) {
          
-     System.out.println("The more we deny that we have a dark side, the more power it has over us.");
+     System.out.println("Becareful of success, it has a dark side.");
      }
 }
